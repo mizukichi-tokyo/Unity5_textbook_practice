ae5003892a0602df3b135f238a0f8185b01c7e80
mizuki kubota <mizuki kubota>
2016-12-05 19:50:18 +0900

    'どうだ'

---
+++ make_thumbnail/Assets/functions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class functions : MonoBehaviour {
+
+    /// <summary>
+    /// Gameobjectのサムネイル画像を作成する
+    /// </summary>
+    /// <param name="go">対象Gameobject</param>
+    /// <param name="savePath">保存先の絶対パス</param>
+    /// <param name="width">サムネイル画像の幅(px)</param>
+    /// <param name="height">サムネイル画像の高さ(px)</param>
+    /// <returns>成功か失敗か</returns>
+    public static bool makeThumbnailPng_from_Gameobject(GameObject go, string savePath, int width, int height)
+    {
+
+
+
+
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ make_thumbnail/Assets/test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    //サムネイルを作成するオブジェクト
+    public GameObject TargetObject;
+
+	// Use this for initialization
+	void Start () {
+        //サムネイル作成
+        functions.makeThumbnailPng_from_Gameobject(TargetObject, "c:/temp/temp1.png", 300, 300);
+    }
+	
+}
